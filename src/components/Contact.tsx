96e9617bea0e60e411680d6d7fbba850f4dab5e1
Olivia Hill <hill.ol@northeastern.edu>
2025-11-06 21:51:12 -0500

    Pushing to git

---
+++ src/components/Contact.tsx
@@ -0,0 +1,15 @@
+export default function Contact() {
+    return (
+        <section id="about" className="about-section">
+            <div className="about-container">
+                <h2 className="section-title">About Us</h2>
+                <p className="about-text">
+                    Hi! I'm Olivia, a second year Computer Science and Mathematics major.
+                </p>
+                <p className="about-text">
+                    Something else about how much she likes doing nails/backstory.
+                </p>
+            </div>
+        </section>
+    );
+}
