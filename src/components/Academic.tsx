6a18379ef2a3c2d40ecc2160682264bdd0ba2d6f
Olivia Hill <hill.ol@northeastern.edu>
2025-11-09 23:18:50 -0500

    css

--- src/components/Academic.tsx
+++ src/components/Academic.tsx
@@ -2,6 +2,9 @@ export default function Academic() {
     return (
         <section id="academic" className="academic-section">
             <div className="academic-flex-container">
+                <div className="academic-image-container">
+
+                </div>
                 <div className="academic-container">
                     <h2 className="academic-heading">Current Interests!</h2>
                     <p className="academic-text">Front-end design, React, full stack.</p>
@@ -9,9 +12,6 @@ export default function Academic() {
                     <p className="academic-text">Databases, Algo.</p>
                     <h2 className="academic-heading">A bit more...</h2>
                     <p className="academic-text">why cs + math.</p>
-                </div>
-                <div className="academic-image-container">
-
                 </div>
             </div>
         </section>
