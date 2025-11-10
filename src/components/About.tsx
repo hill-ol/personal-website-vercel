968e6fb3e890613f172e306470cd8ddd366c306c
Olivia Hill <hill.ol@northeastern.edu>
2025-11-09 22:58:05 -0500

    container flex

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,13 +1,17 @@
 export default function About() {
     return (
         <section id="about" className="about-section">
-            <div className="about-container">
-                <img src="/about-me.jpg" alt="About Me" />
-                <h1 className="section-title">Hi!</h1>
-                <p className="about-text">
-                    Hi! I'm Olivia, a second year Computer Science and Mathematics major at Northeastern University.
-                    I'm from the Chicago suburbs.
-                </p>
+            <div className="about-flex-container">
+                <div className="about-container">
+                    <h1 className="section-title">Hi!</h1>
+                    <p className="about-text">
+                        Hi! I'm Olivia, a second year Computer Science and Mathematics major at Northeastern University.
+                        I'm from the Chicago suburbs.
+                    </p>
+                </div>
+                <div className="about-image-container">
+                    <img src="/about-me.jpg" alt="About Me" />
+                </div>
             </div>
         </section>
     );
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,7 +2,7 @@ export default function Contact() {
     return (
         <section id="contact" className="contact-section">
             <div className="contact-container">
-                <h2 className="section-title">Contact</h2>
+                <h2 className="contact-section-title">Contact</h2>
                 <p className="contact-text">
                     Hi! I'm Olivia, a second year Computer Science and Mathematics major at Northeastern University!
 
