b38ef7e904e1687b2e4e4b70aae6ed236bf00bc9
Olivia Hill <hill.ol@northeastern.edu>
2025-11-09 23:32:28 -0500

    css

---
+++ src/components/Personal.tsx
@@ -0,0 +1,32 @@
+export default function Personal() {
+    return(
+        <section id="personal" className="personal-section">
+            <div className="personal-flex-container">
+                <div className="personal-container">
+                    <h2 className="personal-heading">Outside of the classroom</h2>
+                    <p className="personal-text">
+                        Hi! I'm Olivia, a second year Computer Science and Mathematics major at Northeastern University.
+                        I'm from the Chicago suburbs.
+                    </p>
+                    <h2 className="personal-heading">Things I love</h2>
+                    <p className="personal-text">
+                        Hi! I'm Olivia, a second year Computer Science and Mathematics major at Northeastern University.
+                        I'm from the Chicago suburbs.
+                    </p>
+                </div>
+                <div className="photo-grid">
+                    <h2 className="photo-album-title">Little Photo Album:</h2>
+                    <img src="/ashrita-olivia-yosemite.jpeg" alt="Two girls in Yosemite" />
+                    <img src="/cape-photo-stack.JPG" alt="Girls at cape" />
+                    <img src="/chicago-photo.jpeg" alt="Girls in Chicago" />
+                    <img src="/croatia.jpeg" alt="Croatia" />
+                    <img src="/family-photo.jpeg" alt="My family!" />
+                    <img src="/Kappa%20Delta.jpg" alt="My sorority" />
+                    <img src="/Kappa%20Delta.jpg" alt="My sorority" />
+                    <img src="/Kappa%20Delta.jpg" alt="My sorority" />
+                    <img src="/Kappa%20Delta.jpg" alt="My sorority" />
+                </div>
+            </div>
+        </section>
+    );
+}
